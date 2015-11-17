3274fd2c234e8d814ed7f92539c9b879f9c7027b
benwalcutt <bewalcut@email.uark.edu>
2015-11-17 12:43:28 -0600

    test

--- src/main/java/org/softeng/project/hb_server/resources/products.java
+++ src/main/java/org/softeng/project/hb_server/resources/products.java
@@ -3,6 +3,7 @@ package org.softeng.project.hb_server.resources;
 import java.util.List;
 import java.util.UUID;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -11,7 +12,9 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.xml.bind.JAXBElement;
 
 import org.softeng.project.hb_server.model.product;
@@ -21,6 +24,19 @@ import org.softeng.project.hb_server.services.ProductService;
 public class products {
 	
 	ProductService productService = new ProductService();
+	
+	@GET
+	@Path("/demo")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getDemo() {
+		
+		Response response = Response.status(200)
+				.entity("{\"Name\":\"Ben\"}")
+				.header("Access-Control-Allow-Origin", "*")
+				.header("Content-Type", "application/json")
+				.build();
+		return response;
+	}
 	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
